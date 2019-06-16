8b18c7b9437693070603c51b6600f770c018dac0
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2019-06-16 16:36:37 -0700

    brand new

---
+++ app.js
@@ -0,0 +1,17 @@
+// var typewriterEffect = require("typewriter-effect");
+
+// var app = document.getElementById('developer');
+
+// var typewriter = new Typewriter(app, {
+//     loop: true
+// });
+
+// typewriter.typeString('Hello World!')
+//     .pauseFor(2500)
+//     .deleteAll()
+//     .typeString('Full Stack Developer')
+//     .pauseFor(2500)
+//     .deleteAll()
+//     .typeString('<strong>Front End Developer</strong>')
+//     .pauseFor(2500)
+//     .start();
